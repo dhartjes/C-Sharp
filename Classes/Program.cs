a3d86ad1cbae48654355a983cf0229d75cca6d23
dhartjes <dhartjes@gmail.com>
2014-09-05 10:51:21 -0500

    Simple Classes Program with Getters and Setters

---
+++ Classes/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Employee
+    {
+        private int age;
+        public int MyAge()
+        {
+            return age;
+        }
+        public void SetAge(int someAge)
+        {
+            age = someAge;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee Dhartjes = new Employee();
+            Dhartjes.SetAge(32);
+            int age = Dhartjes.MyAge();
+            Console.WriteLine("Dave's age is {0}", age);
+        }
+    }
+}
