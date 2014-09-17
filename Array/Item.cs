f92a8520f96910995b378f7a8348f2bc91330c99
dhartjes <dhartjes@gmail.com>
2014-09-17 15:50:58 -0500

    Arrays

---
+++ Array/Item.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Arrays
+{
+    class Item
+    {
+        public int ID { get; set; }
+        public Item(int id)
+        {
+            ID = id;
+        }
+    }
+}
