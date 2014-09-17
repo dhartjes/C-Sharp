53ef1194efb4e6b6f3d6b63820e75cd4472cfec6
dhartjes <dhartjes@gmail.com>
2014-09-17 15:57:38 -0500

    Demo2

---
+++ CSSF2Demo/InsuranceInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSSF2Demo
+{
+    public class InsuranceInfo
+    {
+        public string PolicyName { get; set; }
+        public string PolicyID { get; set; }
+    }
+}
