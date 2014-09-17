7cbc77f0e8f15d7d0a1b6cfc123f4a920a54fcc5
dhartjes <dhartjes@gmail.com>
2014-09-17 16:16:34 -0500

    WhileLoops

---
+++ WhileLoops/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How old are you? ");
+            int age = int.Parse(Console.ReadLine());
+
+            do
+            {
+                Console.Write("Happy Birthday ");
+                age--;
+            } while (age > 0);
+
+            Console.WriteLine("/nTaDa!!");
+        }
+    }
+}
