f4f299f15c1d380a826244981dfa87dbf5232e5e
dhartjes <dhartjes@gmail.com>
2014-09-17 16:06:47 -0500

    Demo3

---
+++ CSSFSDemo3/Printable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSSFSDemo3
+{
+    interface Printable //Doesn't use access identifiers like public, private, protected or whatever. Not legal.
+    {
+        //All implementing classes must implement both a metohd and a property.
+        void Print();
+        int Status { get; }
+    }
+}
---
+++ CSSFSDemo3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSSFSDemo3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myDoc = new Document();
+            myDoc.Print();
+            myDoc.CountCharacters();
+            var status = myDoc.Status;
+            Console.WriteLine(status);
+
+            //You cannot instantiate an Interface.
+            //Printable mySecondDoc = new Printable();
+
+            //You can however instantiate an interface if you initialize it from an object that implements the interface.
+            Printable printableItem = myDoc;
+            //Then you can call methods included in the interface on this object, but not methods associated with Document.
+            printableItem.Print();
+
+            //This fails on the other hand.
+            //printableItem.CountCharacters();
+
+            //Why do we implement an interface? Perhaps we got printableItem passed into us as the paramater of a method and we know that it is a document.
+            //We can cast!
+            Document theDoc = printableItem as Document;
+            //When casting, check for null.
+            if (theDoc != null)
+                theDoc.CountCharacters();
+
+            string contents = theDoc.Read();
+            Console.WriteLine("contents: " + contents);
+            theDoc.Write();
+
+        }
+    }
+}
