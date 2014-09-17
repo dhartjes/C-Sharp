840ebdbdb6052c2c38226e65c227965d5a8d0ec4
dhartjes <dhartjes@gmail.com>
2014-09-17 16:05:07 -0500

    Demo2

---
+++ CSSFSDemo2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSSFSDemo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // Control c = new Control(); It's not legal to create an instance of an abstract class.
+            Control button = new Button(0, 0, "Click Me!");
+            Control textBlock = new TextBlock(20, 20, "Hello World");
+
+            List<Control> controls = new List<Control>();
+            controls.Add(button);
+            controls.Add(textBlock);
+
+            foreach (Control ctrl in controls)
+            {
+                ctrl.DrawMe();
+            }
+        }
+    }
+}
