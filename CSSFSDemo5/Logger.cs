5ee29ae40e3a93d8409fdfe2c14c22b5aec71deb
dhartjes <dhartjes@gmail.com>
2014-09-17 15:30:02 -0500

    Subscribe and Publish Events

---
+++ CSSFSDemo5/Logger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSSFSDemo5
+{
+    public class Logger
+    {
+        public void Subscribe(Clock theClock)
+        {
+            theClock.TimeChanged += new Clock.TimeChangeHandler(NewTime);
+        }
+
+
+        public void NewTime(object theClock, TimeEventArgs e)
+        {
+            Console.WriteLine("Logging event at {0}:{1}:{2}",
+                e.Hour.ToString(),
+                e.Minute.ToString(),
+                e.Second.ToString());
+        }
+    }
+}
