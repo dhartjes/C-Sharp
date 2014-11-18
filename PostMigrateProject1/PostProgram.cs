140e83c81d5b64eadf38a5646a700eb45c2557b1
dhartjes <dhartjes@gmail.com>
2014-11-18 15:37:59 -0600

    One database linked

---
+++ PostMigrateProject1/PostProgram.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostMigrateProject1
+{
+    public class PostProgram : IDisposable
+    {
+        static void Main(string[] args)
+        { }
+
+        public int updateDatabase(string queryString)
+        {
+            int status;
+
+            using (Entities2 _db = new Entities2())
+            {
+                var entity = _db.New_ChangeOrder.FirstOrDefault(x => x.ID == 2024);
+                entity.RequestedWork = queryString;
+                status = _db.SaveChanges();
+            }
+            return status;
+        }
+
+    }
+}
---
+++ PreMigrateProject1/Program.cs
@@ -0,0 +1,34 @@
+﻿using PostMigrateProject1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace PreMigrateProject1
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            string longAssString;
+
+            using (Entities1 _db = new Entities1())
+            {
+                longAssString = _db.ChangeOrders
+                    .Where(x => x.ID == 348)
+                    .Select(x => x.RequestedWork)
+                    .FirstOrDefault();
+            }
+
+            int status;
+
+            using (PostProgram pp = new PostProgram()){
+               status = pp.updateDatabase(longAssString);
+            }
+            Console.WriteLine(status);
+            return status;
+        }
+    }
+}
